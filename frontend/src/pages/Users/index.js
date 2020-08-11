dbb394e6d2302cb978cc8b755086c084079d189e
camposyuri <yurinapoleao@gmail.com>
2020-08-11 08:38:37 -0300

    [ADD] - Create function delete and validation if exists users or no

--- frontend/src/pages/Users/index.js
+++ frontend/src/pages/Users/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../../services/";
-import { Container, Button } from "reactstrap";
+import { Container, Button, Alert } from "reactstrap";
 import TableList from "../../components/TableList";
 import TableHeader from "../../components/TableList/TableHeader";
 import TableBody from "../../components/TableList/TableBody";
@@ -9,20 +9,33 @@ import { Link } from "react-router-dom";
 const Users = () => {
   const [data, setData] = useState([]);
 
-  // GET: Listando minha API
+  // GET: Listing my users of API
   useEffect(() => {
     api.get("/users").then((res) => {
       setData(res.data);
     });
   }, []);
 
+  // Function delete users
   const deleteUsers = (id) => {
-    console.log(id);
-    api.delete(`/api/users/${id}`).then((res) => {
-      const filters = data.filter((item) => item.id !== id);
-      setData(filters);
+    api.delete("/users/" + id).then((res) => {
+      const filtrado = data.filter((item) => item.id !== id);
+      setData(filtrado);
     });
   };
+
+  // When not exists register users
+  if (data.length === 0) {
+    return (
+      <Container>
+        <h1>Users</h1>
+        <Link className="btn btn-primary my-2" to="/newUsers">
+          New users
+        </Link>
+        <Alert color="warning">You it hasn't users create</Alert>
+      </Container>
+    );
+  }
 
   return (
     <>
@@ -31,8 +44,9 @@ const Users = () => {
         <Link className="btn btn-primary my-2" to="/newUsers">
           New users
         </Link>
+
         <TableList>
-          <TableHeader name="Name" email="E-mail" actions="Actions" />
+          <TableHeader id="ID" name="Name" email="E-mail" actions="Actions" />
           {data.map((user) => {
             return (
               <TableBody
@@ -41,7 +55,7 @@ const Users = () => {
                 name={user.name}
                 email={user.email}
               >
-                <Link className="btn btn-warning" to={"/api/users" + user.id}>
+                <Link className="btn btn-warning" to={"/users" + user.id}>
                   Edit
                 </Link>
                 <Button
